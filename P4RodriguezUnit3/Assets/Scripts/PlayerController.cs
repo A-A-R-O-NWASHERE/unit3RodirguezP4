93941c891cc901e187ec9141c0dd18aa2c3542cb
A-A-R-O-NWASHERE <768335@student.dusd.net>
2026-04-02 11:24:42 -0700

    movement
    moving around

--- P4RodriguezUnit3/Assets/Scripts/PlayerController.cs
+++ P4RodriguezUnit3/Assets/Scripts/PlayerController.cs
@@ -3,17 +3,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    private Rigidbody playerRB;
+    private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerRB = GetComponent<Rigidbody>();
-        playerRB.AddForce(Vector3.up * 1000);
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
